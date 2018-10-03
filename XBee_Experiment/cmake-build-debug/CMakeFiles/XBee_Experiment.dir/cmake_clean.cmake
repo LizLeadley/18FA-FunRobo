9753fb533fa7a3af8e974b2e3197b704dad9ca03
LizLeadley <elizabeth.leadley@students.olin.edu>
2018-10-03 16:40:24 -0400

    added XBee experiment file

---
+++ XBee_Experiment/cmake-build-debug/CMakeFiles/XBee_Experiment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XBee_Experiment.dir/main.cpp.o"
+  "XBee_Experiment.pdb"
+  "XBee_Experiment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XBee_Experiment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
