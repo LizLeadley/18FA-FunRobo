fe4bdd9be6a653aa58c5c91742207d8fc08f75ab
LizLeadley <elizabeth.leadley@students.olin.edu>
2018-10-08 16:16:25 -0400

    over-comment Sense code for sharing

--- LabThinkSenseArduinoCode/LabThinkSenseArduinoCode.ino
+++ LabThinkSenseArduinoCode/LabThinkSenseArduinoCode.ino
@@ -1,20 +1,22 @@
 ////////////////////////////////////////////////
 //Title: Sense Arduino
 //Description: Sense Slave Arduino, serially connected (w/ digital pins
-////12,13) to Master Arduino Think
-//Robot Name?: TugBoat
+////12,13) to Master Arduino Think. Connected to Sharp IRs on pins 0-4,
+////see below for order. 
+//Robot Name: TugBoat
 //What does code do: Code will simple serial connect Sense Arduino
 ////to Think arduino (not act) sending data and recieving commands
 //Hardware Warnings:
-//Created by Team Think
+//Created by Team Pixy-Tug
 ////////////////////////////////////////////////
+
 
 ///////////////////////////////
 //Libraries
 ///////////////////////////////
 #include <SoftwareSerial.h>  //includes software serial library
 #include <SPI.h>             // pixystuff
-#include <Pixy.h>
+#include <Pixy.h>            // more pixystuff
 #include <math.h>            // math library
 ///////////////////////////////
 
@@ -23,11 +25,11 @@ SoftwareSerial senseSerial(12, 13);     //connects sense arduino to think arduin
 
 // All the variables we need
 Pixy pixy;                     // This is the main Pixy object
-int xValue;                    // x value of the cetner of the box, seen by the Pixy Cam
+int xValue;                    // x value of the center of the box, seen by the Pixy Cam
 int offsetDeg;                 // position in degrees with 0 degrees = straight
 float ratio;                   // value used in the remap function
 String newValue;               // return value used for round off function
-String strValue;               // string used to make a 3 digit string of the angle 
+String strValue;               // string used to make a 3 digit string of the angle
 String angleStr;               // String of the angle to put in the string sent over serial
 String serialStr;              // String to send serial data
 unsigned long previousMillis = 0;
@@ -46,10 +48,10 @@ int j = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  senseSerial.begin(19200);               //sets baud rate for sense to 9600
+  senseSerial.begin(19200);               //sets baud rate for sense to 19200
   //senseSerial has same name in think and sense arduinos
   pixy.init();                           // pixy initialization
-  Serial.begin(9600);
+  Serial.begin(9600);                    // starts separate serial with computer at 9600
   Serial.print("Starting...\n");
 
 }
@@ -65,7 +67,7 @@ void loop() {
 
   unsigned long currentMillis = millis();  // Millis initialization
   static int i = 0;                        // Random pixy variables I don't know
-  uint16_t blocks;
+  uint16_t blocks;                         // see pixy tutorial link in lab doc
   char buf[32];
 
   // grab blocks!
@@ -75,7 +77,7 @@ void loop() {
   if (blocks)
   {
     i++;
-    // do this (print) every 50 frames because printing every
+    // do this (print) every 10 frames because printing every
     // frame would bog down the Arduino
     if (i % 10 == 0)
     {
@@ -93,13 +95,13 @@ void loop() {
   // Sharp IR's code
   
   left = roundOff(valToDist(analogRead(analogPin0)));               // reading the analogpin, convert to distance and round off to interval between [15,99]
-  right = roundOff(valToDist(analogRead(analogPin1)));
+  right = roundOff(valToDist(analogRead(analogPin1)));              // distances are in centimeters
   softLeft = roundOff(valToDist(analogRead(analogPin2)));
   softRight = roundOff(valToDist(analogRead(analogPin3)));          
   back = roundOff(valToDist(analogRead(analogPin4))); 
   
   serialStr =  angleStr + left + softLeft + softRight + right + back;   // Putting together the string to send through serial
-  Serial.println(serialStr);                                        // See in serial what's it sending through
+  Serial.println(serialStr);                                            // See in serial what's it sending through
 
   
   
@@ -107,21 +109,11 @@ void loop() {
   //Basic softSerial Code
   ////////////////////////////////////////
 
-  senseSerial.println(serialStr);          //Prints data to the transmit pin of the Sense software serial port ###Note: This never worked, so maybe there is a better way?
+  senseSerial.println(serialStr);
+  //Prints data to the transmit pin of the Sense software serial port
+  //###Note: This never worked, so maybe there is a better way?
   ////////////////////////////////////////
 
-  ////////////////////////////////////////
-  //Basic Sense Actions Following
-  ////////////////////////////////////////
-
-  //read data from think arduino  (probably passive)
-  //what data need to be sent to sense arduino?
-  //sensor initialization?
-  //if sense unusual print specific thing?
-
-  //send data to think arduino    (probably active)
-  //different data sent for IR sensors and pixy and other
-  ////////////////////////////////////////
 }
 
 
@@ -129,31 +121,32 @@ void loop() {
 //Functions
 /////////////////////////////////////////////////////
 
-// Remap function between 2 intervals  ###Note: Actually there is a built-in function in arduino that also does this for you ('map')
+// Remap function between two intervals
+//###Note: Actually there is a built-in function in arduino that also does this for you ('map')
 float remapInterval(float value, float int1_start, float int1_end, float int2_start, float int2_end)
 {
-  ratio = (value - int1_start) / (int1_end - int1_start);
-  return (int2_start + ratio * (int2_end - int2_start));
+  ratio = (value - int1_start) / (int1_end - int1_start); //Convert the value into a ratio out of the initial total interval size
+  return (int2_start + ratio * (int2_end - int2_start));  //Add the start value of the second interval, to the ratio mapped onto the new total interval size
 }
 
 // IR reading to cm function
 int valToDist(int val)
 {
-  return pow((3027.4 / val), 1.2134);
+  return pow((3027.4 / val), 1.2134);           //Uses the math library's function pow (power, as in exponents)
 }
 
 // Function rounding of value to [15,99] and make a 2 digit string
 String roundOff(int val)
 {
-  if (val > 99)
+  if (val > 99)       // if the value is greater than 99, truncate it to exactly 99
   {
     newValue = "99";
   }
-  else if (val < 15)
+  else if (val < 15)  // if the value is less than 15, set it to 00 to be ignored
   {
     newValue = "00";
   }
-  else
+  else                // if the value is within range, make it a string for serial handling
   {
     newValue = String(val);
   }
@@ -161,7 +154,8 @@ String roundOff(int val)
 }
 
 // Function making a 3-digit string of the angle value, with the first digit (0,1) = (-,+) respectively
-// ### Note: We first tried to first make a string and check if the first char was "-", which it didn't want to do for some reason. That's why we make a string in both if-statements.
+// ### Note: We first tried to first make a string and check if the first char was "-", which it didn't want to do for some reason.
+    // That's why we make a string in both if-statements.
 String makeString(int val)
 {
   if (val < 0)                      // Turns the - into 0
